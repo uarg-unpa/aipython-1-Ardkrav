0c9e07f9a68ae9826cc51bf19997b9fe271e51bd
Collareda Agustin <161783100+Ardkrav@users.noreply.github.com>
2024-03-07 00:27:22 +0000

    Avance de los ejercicios 14 a 22 de la guia 1

---
+++ entrega/guia1/ejercicio_16.py
@@ -0,0 +1,4 @@
+cadena = "sometimes it is the people no one imagines anything of who do the things that no one can imagine."
+print(cadena.capitalize())
+print(cadena.title())
+print(cadena.swapcase())
---
+++ entrega/guia1/ejercicio_17.py
@@ -0,0 +1,2 @@
+nombre_completo = input("Ingrese su nombre completo")
+print(nombre_completo*3)
